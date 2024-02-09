596c17ea49909836b214ae234493c5ce9b350e75
AgredaLuis <Luis.Agreda98@gmail.com>
2024-02-09 13:41:37 -0400

    refact  style img

--- src/components/CodeQR.tsx
+++ src/components/CodeQR.tsx
@@ -2,13 +2,13 @@ import { qrCode } from "../assets";
 import { Paragraph } from "./atoms";
 export default function CodeQR() {
   return (
-    <div className="max-w-[290px]  mx-auto bg-white rounded-[28px] p-3 my-custom-shadow">
+    <div className="max-w-[290px]  mx-auto bg-white rounded-[20px] p-3 my-custom-shadow">
       <img
         src={qrCode}
         alt="qr-code-to-Frontend-Mentor"
-        className="mx-auto w-[260px] h-[260px] object-cover overflow-hidden rounded-2xl"
+        className="mx-auto w-[260px] h-[260px] object-cover overflow-hidden rounded-lg"
       />
-      <div className="text-center px-2 py-2">
+      <div className="text-center px-2 py-5">
         <h1 className="text-darkBlue pb-3 font-bold font-outfit text-xl">
           Improve your front-end skills by building projects
         </h1>
